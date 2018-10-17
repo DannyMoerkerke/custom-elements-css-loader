678d176948562928f091ebb73f68b8effcace871
Danny Moerkerke <danny@dannymoerkerke.nl>
2018-10-17 23:59:16 +0200

    refactored all regex logic to do/while loops to remove duplicate code

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const prefixer = postcss([autoprefixer({
     grid: true
 })]);
 
-module.exports = function(source) {
+module.exports = source => {
     const styleRegex = /<style>([\s\S]*?)<\/style>/gm;
     let styleResult = styleRegex.exec(source);
 
@@ -21,7 +21,7 @@ module.exports = function(source) {
     }
 
     const tagNameRegex = /customElements\.define\('(.+?)'/gm;
-    const tagName = tagNameRegex.exec(source);
+    const tagName = tagNameRegex.exec(source)[1];
 
     // matches all rules that do not start with "from" or "to" (which are keyframe rules)
     const ruleRegex = /^(?!\s*(from|to))([\sa-zA-Z0-9\-_*()\[\]#.:,]*?){([\s\S]*?)}$/gm;
@@ -35,82 +35,74 @@ module.exports = function(source) {
 
     // const callback = this.async();
 
+    const orgStyle = styleResult[1];
     let style = styleResult[1];
 
-    let ruleResult = ruleRegex.exec(style);
+    /**
+     * Prefix all regular rules (not :host or ::slotted) with the tagname of the custom element
+     */
+    let ruleResult;
 
-    if(ruleResult) {
-        // ruleResult
-        let ruleStartResult = ruleStartRegex.exec(ruleResult[0]);
+    do {
+        ruleResult = ruleRegex.exec(style);
 
-        if(ruleStartResult) {
-            // ruleStartResult
-            let notHostResult = notHostOrSlottedRegex.exec(ruleStartResult[2]);
+        if(ruleResult) {
+            let ruleStartResult = ruleStartRegex.exec(ruleResult[0]);
 
-            if(notHostResult) {
-                // notHostResult
-                const selectors = ruleStartResult[2].split(',');
-                // prefixes all rules with the tagname of the custom element
-                const replacement = selectors.map(selector => `${tagName[1]} ${selector.trim()}`).join(', ');
-                style = style.replace(ruleResult[0].trim(), `${replacement} {${ruleResult[3]}}`);
-            }
-        }
+            if(ruleStartResult) {
+                let notHostResult = notHostOrSlottedRegex.exec(ruleStartResult[2]);
 
-        while(ruleRegex.lastIndex) {
-            ruleResult = ruleRegex.exec(style);
-            // ruleResult
-            if(ruleResult) {
-                let ruleStartResult = ruleStartRegex.exec(ruleResult[0]);
-                // ruleStartResult
-                if(ruleStartResult) {
-                    let notHostResult = notHostOrSlottedRegex.exec(ruleStartResult[2]);
-                    if(notHostResult) {
-                        // notHostResult
-                        const selectors = ruleStartResult[2].split(',');
-                        // selectors
-                        const replacement = selectors.map(selector => `${tagName[1]} ${selector.trim()}`).join(', ');
-                        // replacement
-                        style = style.replace(ruleResult[0].trim(), `${replacement} {${ruleResult[3]}}`);
-                    }
+                if(notHostResult) {
+                    const selectors = ruleStartResult[2].split(',');
+                    const replacement = selectors.map(selector => `${tagName} ${selector.trim()}`).join(', ');
+                    style = style.replace(ruleResult[0].trim(), `${replacement} {${ruleResult[3]}}`);
                 }
             }
         }
-    }
+    } while(ruleResult);
 
-    let slottedResult = slottedRegex.exec(style);
 
-    if(slottedResult) {
-        // replaces ::slotted with the tagname of the custom element
-        style = style.replace(slottedResult[0], `${tagName[1]} ${slottedResult[1]}`);
+    /**
+     * Replace all ::slotted rules with the tagname of the custom element
+     */
+    let slottedResult;
 
-        while(slottedRegex.lastIndex) {
-            slottedResult = slottedRegex.exec(style);
-            if(slottedResult) {
-                style = style.replace(slottedResult[0], `${tagName[1]} ${slottedResult[1]}`);
+    do {
+        slottedResult = slottedRegex.exec(style);
+
+        if(slottedResult) {
+            style = style.replace(slottedResult[0], `${tagName} ${slottedResult[1]}`);
+
+            while(slottedRegex.lastIndex) {
+                slottedResult = slottedRegex.exec(style);
+                if(slottedResult) {
+                    style = style.replace(slottedResult[0], `${tagName} ${slottedResult[1]}`);
+                }
             }
         }
-    }
+    } while(slottedResult);
 
-    let hostResult = hostRegex.exec(style);
 
-    if(hostResult) {
-        if(hostResult[2] === undefined) {
-            hostResult[2] = '';
-        }
-        if(hostResult[3] === undefined) {
-            hostResult[3] = hostResult[2] === '' ? ' ' : '';
-        }
-        style = style.replace(hostResult[0], `${tagName[1]}${hostResult[2]}${hostResult[3]}{`);
+    /**
+     * Replace all :host rules with the tagname of the custom element
+     */
+    let hostResult;
 
-        while(hostRegex.lastIndex) {
-            hostResult = hostRegex.exec(style);
-            if(hostResult) {
-                style = style.replace(hostResult[0], `${tagName[1]}${hostResult[2]}${hostResult[3]}{`);
+    do {
+        hostResult = hostRegex.exec(style);
+
+        if(hostResult) {
+            if(hostResult[2] === undefined) {
+                hostResult[2] = '';
             }
+            if(hostResult[3] === undefined) {
+                hostResult[3] = hostResult[2] === '' ? ' ' : '';
+            }
+            style = style.replace(hostResult[0], `${tagName}${hostResult[2]}${hostResult[3]}{`);
         }
-    }
+    } while(hostResult);
 
-    source = source.replace(styleResult[1], style);
+    source = source.replace(orgStyle, style);
 
     return source;
 
